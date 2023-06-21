a3dcb4a21d7630c81e62fae088f245359deb646b
hassanromhuda <132324459+hassanromhuda@users.noreply.github.com>
2023-06-21 17:53:19 +0800

    Touch up gaussian likelihood derivation.

--- models.py
+++ models.py
@@ -488,8 +488,10 @@ class SynthesizerTrn(nn.Module):
       #                                      = -0.5 * ln(2π) - logs_p - (f(x)-m_p)² * (0.5 * exp(- 2 log σ))
       #                                      = -0.5 * ln(2π) - logs_p - 0.5 * (f(x)-m_p)² * exp(- 2 log σ)
       #                                      = -0.5 * ln(2π) - logs_p - 0.5 * (f(x)-m_p)² * sp_sq_r
-      #                                      = -0.5 * ln(2π) - logs_p - 0.5 * (f^2(x)-m^2_p - 2 * f(x) * m_p) * sp_sq_r
-      #                                      = -0.5 * ln(2π) - logs_p - (0.5 * f^2(x) - 0.5 * m^2_p - f(x) * m_p) * sp_sq_r
+      #                                      = -0.5 * ln(2π) - logs_p - 0.5 * (f²(x) + m_p² - 2 * f(x) * m_p) * sp_sq_r
+      #                                      = -0.5 * ln(2π) - logs_p + (-0.5 * f²(x) - 0.5 * m_p² + f(x) * m_p) * sp_sq_r
+      #                                      = (-0.5 * ln(2π) - logs_p) + (-0.5 * f²(x) * sp_sq_r) + (-0.5 * m_p² * sp_sq_r) + (f(x) * m_p * sp_sq_r)
+      #                                      = neg_cent1 + neg_cent2 + neg_cent4 + neg_cent3 
 
       s_p_sq_r = torch.exp(-2 * logs_p)                                               # [b, d, T_src]
       neg_cent1 = torch.sum(-0.5 * math.log(2 * math.pi) - logs_p, [1], keepdim=True) # [b, 1, T_src] # -0.5 * ln(2π) - logs_p
